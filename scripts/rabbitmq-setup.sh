86dde9924041e810811dd8621c4774872c73f2f3
marpierc <marpierc@iu.edu>
2016-05-16 15:26:43 -0400

    Tweak

--- scripts/rabbitmq-setup.sh
+++ scripts/rabbitmq-setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #
-# Setup RabbitMQ user, vhost and permissions.
+# Setup RabbitMQ user, vhost and permissions. This script must be run by a
+# privileged user.
 #
+echo "Setting up RabbitMQ user, vhosts and permissions"
 sudo rabbitmqctl add_user airavata airavata
 sudo rabbitmqctl add_vhost messaging
 sudo rabbitmqctl set_permissions -p messaging airavata "." "." ".*"
